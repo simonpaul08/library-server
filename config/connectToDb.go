986da7d69ba8ac09f3243e2cf36eb19cf0b3787f
Simon Paul <samonpaul06@gmail.com>
2025-04-17 10:09:49 +0530

    feat: added db env and vercel config

--- config/connectToDb.go
+++ config/connectToDb.go
@@ -2,9 +2,13 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"project/libraryManagement/models"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"project/libraryManagement/models"
 )
 
 var DB *gorm.DB
@@ -12,7 +16,16 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 
-	dt := "postgres://postgres:postgres@localhost:5432/library?connect_timeout=10"
+	envError := godotenv.Load(".env")
+
+	if envError != nil {
+		log.Fatalf("Error loading .env file")
+		return
+	}
+
+	db_url := (os.Getenv("DATABASE_URL"))
+
+	dt := db_url
 	DB, err = gorm.Open(postgres.Open(dt), &gorm.Config{})
 
 	if err != nil {
@@ -28,4 +41,3 @@ func ConnectToDB() {
 	fmt.Println("Connected To Database")
 }
 
-// postgres://ohfvvnou:sJ1qyOSbStI57eDBbC8dOVXNQI1OWwZj@tyke.db.elephantsql.com/ohfvvnou
